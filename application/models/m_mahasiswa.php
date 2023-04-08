887b1c589af01bd36ed9041c151d9fd6515f4cda
arifanchan <19215277@bsi.ac.id>
2023-04-09 00:16:49 +0700

    tugas 4 action edit, ModelUser, ModelBuku

--- application/models/m_mahasiswa.php
+++ application/models/m_mahasiswa.php
@@ -26,4 +26,13 @@ class m_mahasiswa extends CI_Model
     //{ 
     //    $this->db->insert('mahasiswa', $data); 
     //}
+    function edit_data($where,$table)
+    {		
+	    return $this->db->get_where($table,$where);
+    }
+    function update_data($where,$data,$table)
+    {
+		$this->db->where($where);
+		$this->db->update($table,$data);
+	}	
 }
